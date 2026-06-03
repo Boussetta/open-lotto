0a25914324031e29104224483c9b508f0648ea43
Wissem Boussetta <wissem.boussetta@outlook.com>
2026-06-03 14:23:09 +0200

    stop tracking build directory.

--- build/CMakeFiles/eurojackpot.dir/plugins/eurojackpot.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/eurojackpot.dir/plugins/eurojackpot.c.o: \
- /home/wissem/workspace/open-lotto/plugins/eurojackpot.c \
- /usr/include/stdc-predef.h \
- /home/wissem/workspace/open-lotto/include/combogen.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stddef.h \
- /home/wissem/workspace/open-lotto/include/lottery_plugin.h \
- /home/wissem/workspace/open-lotto/include/combogen.h
--- build/CMakeFiles/lotto.dir/plugins/lotto.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/lotto.dir/plugins/lotto.c.o: \
- /home/wissem/workspace/open-lotto/plugins/lotto.c \
- /usr/include/stdc-predef.h \
- /home/wissem/workspace/open-lotto/include/combogen.h \
- /usr/lib/gcc/x86_64-linux-gnu/15/include/stddef.h \
- /home/wissem/workspace/open-lotto/include/lottery_plugin.h \
- /home/wissem/workspace/open-lotto/include/combogen.h
